312506a20cd052c904a6da4ba8a5038edd4ba92e
Peter Denev <peter.denev@gmail.com>
2016-02-09 17:02:05 +0200

    add api like standart emitter; on return the callbacl; off can remove specific callbacl

--- FunFire.js
+++ FunFire.js
@@ -28,7 +28,10 @@
 
     var saveHandlers = function(evName, cb, el){
         if(!el.hasOwnProperty(FF)) 
-            el[FF] = { _listeners : {} };        
+            el[FF] = {};
+        if(!el[FF].hasOwnProperty(_l)){
+            el[FF][_l] = {}
+        }             
         if(!el[FF][_l].hasOwnProperty[evName]) 
             el[FF][_l][evName] = [];        
         el[FF][_l][evName].push(cb);
@@ -43,9 +46,20 @@
         return [];
     }
 
-    var clearHandlers = function(el, evName ){
+    var clearHandlers = function(el, evName, cb){
         if(el.hasOwnProperty(FF) && el[FF].hasOwnProperty(_l)){
-           (typeof evName == 'undefined') ? el[FF][_l] = {} : el[FF][_l][evName] = {};            
+           if(typeof evName == 'undefined'){
+                el[FF][_l] = {}
+            }else if(typeof cb === 'undefined'){
+                for(var i in el[FF][_l][evName]){
+                    if(el[FF][_l][evName][i]==cb){
+                        delete el[FF][_l][evName][i];
+                        break;
+                    }
+                }
+            }else{
+                el[FF][_l][evName] = {}; 
+            }
         }
     }
 
@@ -53,7 +67,8 @@
        on : function on(evName, cb, el){
             if(arguments.length<3) el = document; 
             var exec_cb = function(e){
-                var rez = cb(e, e[FF].data, e[FF].callback);                
+                var hasFF = e.hasOwnProperty(FF);                 
+                var rez = cb(e, hasFF ? e[FF].data : null, hasFF ? e[FF].callback : null);           
                 if(rez===false){
                     var hlrs = getHandlers(evName,el);
                     FunFire.off(evName,el);
@@ -69,6 +84,7 @@
                 document.attachEvent("on"+evName, exec_cb);
             }
             saveHandlers(evName, exec_cb, el);
+            return exec_cb;
        },
        emit: function emit(evName, el, data, cb){
             if(arguments.length<2) el = document;
@@ -91,14 +107,38 @@
                 return cb(e, data, f_cb); // call handler
             }, el);           
         },
-        off : function off(evName, el){
+        off : function off(evName, el, cb){
             if(arguments.length<2) el = document;
             var handlers = getHandlers(evName, el);
             for(var h_key in handlers){
-                el.removeEventListener(evName, handlers[h_key]);
+                if(typeof cb === 'undefined' || cb==handlers[h_key]){
+                    el.removeEventListener(evName, handlers[h_key]);
+                }
             }
-            clearHandlers(el, evName );
-        }        
+            clearHandlers(el, evName, cb );
+        },
+        standart: {
+            on: function(evName, cb){
+                var newCb = function(e,data,cb_cb){
+                    return cb.apply(cb,data);
+                }
+                FunFire.on(evName, newCb, window)
+            },
+            emit: function(evName /*, arg1, arg2, ... */){
+                var args = Array.prototype.slice.call(arguments);
+                evName = args.shift(); 
+                return FunFire.emit.apply(this ,[evName, window, args])
+            },
+            once: function(evName, cb){
+                var newCb = function(e,data,cb_cb){
+                    return cb.apply(cb,data);
+                }
+                FunFire.once(evName, newCb, window)
+            },
+            off : function(evName, cb){
+                FunFire.off(evName, window, cb)
+            }
+        } 
     };
 
     return FunFire;
